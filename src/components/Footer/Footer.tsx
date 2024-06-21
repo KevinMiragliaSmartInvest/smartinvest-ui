27e727e7f053a431466f803e9e818a736da26a15
Kevin Miraglia <kmiraglia@smartinvest.technology>
2024-06-21 14:58:23 -0400

    initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() { 
+    return (
+        <div className='full-page-width footer-top'>
+
+            <div></div>
+        </div>
+      );
+}
+
+export default Footer;
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css";
+
+
+function Header() { 
+    return (
+        <div className='full-page-width header-bottom'>
+            <div>
+                <h1>Smart Invest</h1>
+            </div>
+            <div className=""></div>
+        </div>
+      );
+}
+
+export default Header;
+
